0e37add088444011ae684275afe73ac0e945a28e
Jagger <jstrive@foxmail.com>
2024-02-26 23:30:04 +0800

    优化，增加提取标题

---
+++ README.md
@@ -0,0 +1,7 @@
+# 调用方式
+## PDFExtractor调用方式
+```python
+instance = PDFExtractor()
+pdf_path = r'YOUR_PDF_PATH'
+result = instance.extract(pdf_path)
+```
--- app/PDFExtractor.py
+++ app/PDFExtractor.py
@@ -1,218 +1,96 @@
-import PyPDF2
-# 分析PDF的layout，提取文本
-from pdfminer.high_level import extract_pages, extract_text
-from pdfminer.layout import LTTextContainer, LTChar, LTRect, LTFigure, LTTextBoxHorizontal
-# 从PDF的表格中提取文本
 import pdfplumber
-# 从PDF中提取图片
-from PIL import Image
-from pdf2image import convert_from_path
-# 运行OCR从图片中提取文本
-import pytesseract 
-# 清除过程中的各种过程文件
-import os
 import re
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+# 正则表达式匹配中文文本的标题
+title_patterns = [
+    r'^\d+\、.*$',  # 中文数字开头，后接顿号
+    r'^[一二三四五六七八九十]+\、.*$',  # 中文数字开头，后接顿号
+    r'^\d+\..*$',                      # 阿拉伯数字开头，后接点号
+    r'^[\(\（][一二三四五六七八九十]+[\)\）]$',  # 中文括号包裹的中文数字
+    r'^[\(\（]\d+[\)\）].*$',          # 中文括号包裹的阿拉伯数字
+    r'^第[一二三四五六七八九十]+[章节条款] [\u4e00-\u9fa5]+'  # “第三节 管理层讨论与分析”格式
+]
 
 class PDFExtractor():
-    def text_extraction(self, element):
-        # 从行元素中提取文本
-        line_text = element.get_text()
-        if line_text != " \n":
-            print(line_text)
+    def is_bold(self, text_line):
+        first_char = text_line["chars"][0]
+        fontname = first_char["fontname"]
+        
+        # 检查字体名称中是否包含 'Bold' 或类似的术语
+        bold_terms = ['Bold', 'Bd', 'Heavy', 'Black']
+        return any(bold_term in fontname for bold_term in bold_terms)
 
-        # 探析文本的格式
-        # 用文本行中出现的所有格式初始化列表
-        line_formats = []
-        for text_line in element:
-            if isinstance(text_line, LTTextContainer):
-                # 遍历文本行中的每个字符
-                for character in text_line:
-                    if isinstance(character, LTChar):
-                        # 追加字符的font-family
-                        line_formats.append(character.fontname)
-                        # 追加字符的font-size
-                        line_formats.append(character.size)
-        # 找到行中唯一的字体大小和名称
-        format_per_line = list(set(line_formats))
+    def is_within_table(self, text_line, tables):
+        """
+        判断文本行是否在表格中。
 
-        if self.is_heading(line_text, format_per_line):
-            self.headings.append(line_text)
-        # 返回包含每行文本及其格式的元组
-        return (line_text, format_per_line)
-    
-    def is_heading(self, line_text, formats):
-        pattern = r'([一二三四五六七八九十\d]+|[（(][一二三四五六七八九十\d]+[）)])\s*[、.]*\s*'
+        :param text_line: 由 page.extract_text_lines() 返回的文本行字典。
+        :param tables: 由 page.find_tables() 返回的表格列表。
+        :return: 如果文本行在任何表格中，则返回 True；否则返回 False。
+        """
+        line_bbox = (text_line['x0'], text_line['top'], text_line['x1'], text_line['bottom'])
 
-        if re.match(pattern, line_text):
-            # if formats.fontname == "Bold" or formats.fontname.startswith("Bold"):
-            #     if formats.size >= 12 and formats.fontwidth >= 700:  # 字号大于等于12，加粗程度大于等于700
-            #         return True 
-            return True
+        for table in tables:
+            table_bbox = table.bbox  # 表格的边界框 (x0, top, x1, bottom)
+            # 检查文本行和表格边界框是否重叠
+            if (line_bbox[0] < table_bbox[2] and
+                line_bbox[2] > table_bbox[0] and
+                line_bbox[1] < table_bbox[3] and
+                line_bbox[3] > table_bbox[1]):
+                return True  # 文本行在表格中
+                
+        return False  # 文本行不在表格中
+
+    def is_title(self, text_line, tables):
+        # 检查文本行是否在表格中
+        if self.is_within_table(text_line, tables):
+            return False
+
+        # 检查文本行是否符合标题模式
+        for pattern in title_patterns:
+            if re.match(pattern, text_line['text']):
+                return True
 
         return False
 
-    # 创建一个从pdf中裁剪图像元素的函数
-    def crop_image(self, element, pageObj):
-        # 获取从PDF中裁剪图像的坐标
-        [image_left, image_top, image_right, image_bottom] = [element.x0,element.y0,element.x1,element.y1] 
-        # 使用坐标(left, bottom, right, top)裁剪页面
-        pageObj.mediabox.lower_left = (image_left, image_bottom)
-        pageObj.mediabox.upper_right = (image_right, image_top)
-        # 将裁剪后的页面保存为新的PDF
-        cropped_pdf_writer = PyPDF2.PdfWriter()
-        cropped_pdf_writer.add_page(pageObj)
-        # 将裁剪好的PDF保存到一个新文件
-        with open('cropped_image.pdf', 'wb') as cropped_pdf_file:
-            cropped_pdf_writer.write(cropped_pdf_file)
+    def extract_titles(self, text):
+        """
+            从多行文本中，正则匹配所有的符合title的标题
+            缺陷：因为raw_text缺乏位置信息等，结果不如使用extract方法好
+            当前仅用作验证title_patterns单元测试
+        """
+        # 合并所有模式为一个大模式，并捕获匹配的模式及其在文本中的位置
+        combined_pattern = '|'.join(f'({p})' for p in title_patterns)
+        matches = re.finditer(combined_pattern, text, re.MULTILINE)
 
-    # 创建一个将PDF内容转换为image的函数
-    def convert_to_images(self, input_file,):
-        images = convert_from_path(input_file, poppler_path = r"C:\Users\Administrator\Documents\Code\Exam-ElasticSearch\poppler-24.02.0\Library\bin")
-        image = images[0]
-        output_file = "PDF_image.png"
-        image.save(output_file, "PNG")
+        # 提取匹配的标题及其位置
+        titles_with_pos = [(match.group(), match.start()) for match in matches]
 
-    # 创建从图片中提取文本的函数
-    def image_to_text(self, image_path):
-        # 读取图片
-        img = Image.open(image_path)
-        # 从图片中抽取文本
-        text = pytesseract.image_to_string(img)
-        return text
+        # 根据位置排序
+        titles_with_pos.sort(key=lambda x: x[1])
 
-    # 从页面中提取表格内容
-    def extract_table(self, pdf_path, page_num, table_num):
-        # 打开PDF文件
-        pdf = pdfplumber.open(pdf_path)
-        # 查找已检查的页面
-        table_page = pdf.pages[page_num]
-        # 提取适当的表格
-        table = table_page.extract_tables()[table_num]
-        return table
-
-    # 将表格转换为适当的格式
-    def table_converter(self, table):
-        table_string = ''
-        # 遍历表格的每一行
-        for row_num in range(len(table)):
-            row = table[row_num]
-            # 从warp的文字删除线路断路器
-            cleaned_row = [item.replace('\n', ' ') if item is not None and '\n' in item else 'None' if item is None else item for item in row]
-            # 将表格转换为字符串，注意'|'、'\n'
-            table_string+=('|'+'|'.join(cleaned_row)+'|'+'\n')
-        # 删除最后一个换行符
-        table_string = table_string[:-1]
-        return table_string
+        # 只返回标题
+        return [title for title, pos in titles_with_pos]    
 
     def extract(self, pdf_path):
-        # 标题列表
-        self.headings = []
-        # 创建一个PDF文件对象
-        pdfFileObj = open(pdf_path, 'rb')
-        # 创建一个PDF阅读器对象
-        pdfReaded = PyPDF2.PdfReader(pdfFileObj)
-        # 打开pdf文件
-        pdfplumberInstance = pdfplumber.open(pdf_path)
-        extracted_pages = extract_pages(pdf_path)
+        pdf = pdfplumber.open(pdf_path) # pdfplumber 提取表格
 
-        # 创建字典以从每个图像中提取文本
-        text_per_page = {}
-        # 我们从PDF中提取页面
-        for pagenum, page in enumerate(extracted_pages):
-            if pagenum > 25: break
-            # 初始化从页面中提取文本所需的变量
-            pageObj = pdfReaded.pages[pagenum]
-            page_text = []
-            line_format = []
-            text_from_images = []
-            text_from_tables = []
-            page_content = []
-            # 初始化检查表的数量
-            table_num = 0
-            first_element= True
-            table_extraction_flag= False
-            # 查找已检查的页面
-            page_tables = pdfplumberInstance.pages[pagenum]
-            # 找出本页上的表格数目
-            tables = page_tables.find_tables()
+        titles = []
+        page_texts = []
+        for page_num, page in enumerate(pdf.pages):
+            text_lines = page.extract_text_lines() 
+            tables = page.find_tables()
+            
+            for text_line in text_lines:
+                if self.is_title(text_line, tables):
+                    titles.append([text_line["text"], page_num + 1])
 
+            page_texts.append(page.extract_text_simple())
 
-            # 找到所有的元素
-            page_elements = [(element.y1, element) for element in page._objs]
-            # 对页面中出现的所有元素进行排序
-            page_elements.sort(key=lambda a: a[0], reverse=True)
-            # 查找组成页面的元素
-            for i,component in enumerate(page_elements):
-                # 提取PDF中元素顶部的位置
-                pos= component[0]
-                # 提取页面布局的元素
-                element = component[1]
-                
-                # 检查该元素是否为文本元素
-                if isinstance(element, LTTextContainer) or isinstance(element, LTTextBoxHorizontal):
-                    # 检查文本是否出现在表中
-                    if table_extraction_flag == False:
-                        # 使用该函数提取每个文本元素的文本和格式
-                        print('i am here')
-                        (line_text, format_per_line) = self.text_extraction(element)
-                        # 将每行的文本追加到页文本
-                        page_text.append(line_text)
-                        # 附加每一行包含文本的格式
-                        line_format.append(format_per_line)
-                        page_content.append(line_text)
-                    else:
-                        # 省略表中出现的文本
-                        pass
+        return (titles, page_texts)
 
-                # 检查元素中的图像
-                if isinstance(element, LTFigure):
-                    # 从PDF中裁剪图像
-                    self.crop_image(element, pageObj)
-                    # 将裁剪后的pdf转换为图像
-                    self.convert_to_images('cropped_image.pdf')
-                    # 从图像中提取文本
-                    image_text = self.image_to_text('PDF_image.png')
-                    text_from_images.append(image_text)
-                    page_content.append(image_text)
-                    # 在文本和格式列表中添加占位符
-                    page_text.append('image')
-                    line_format.append('image')
-
-                # 检查表的元素
-                if isinstance(element, LTRect):
-                    # 如果第一个矩形元素
-                    if first_element == True and (table_num+1) <= len(tables):
-                        # 找到表格的边界框
-                        lower_side = page.bbox[3] - tables[table_num].bbox[3]
-                        upper_side = element.y1 
-                        # 从表中提取信息
-                        table = self.extract_table(pdf_path, pagenum, table_num)
-                        # 将表信息转换为结构化字符串格式
-                        table_string = self.table_converter(table)
-                        # 将表字符串追加到列表中
-                        text_from_tables.append(table_string)
-                        page_content.append(table_string)
-                        # 将标志设置为True以再次避免该内容
-                        table_extraction_flag = True
-                        # 让它成为另一个元素
-                        first_element = False
-                        # 在文本和格式列表中添加占位符
-                        page_text.append('table')
-                        line_format.append('table')
-
-                        # 检查我们是否已经从页面中提取了表
-                        if element.y0 >= lower_side and element.y1 <= upper_side:
-                            pass
-                        elif not isinstance(page_elements[i+1][1], LTRect):
-                            table_extraction_flag = False
-                            first_element = True
-                            table_num+=1
-
-            text_per_page[pagenum]= [page_text, line_format, text_from_images,text_from_tables, page_content]
 if __name__ == "__main__":
     instance = PDFExtractor()
     pdf_path = r'C:\Users\Administrator\Downloads\阿莱德 2022年年度报告摘要.pdf'
-    instance.extract(pdf_path)
-    print(instance.headings)
+    result = instance.extract(pdf_path)
+    print(result)
